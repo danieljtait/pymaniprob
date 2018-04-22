6d2c1c0a9c00b5e50461a8fd15da46e7dafc844d
danieljtait <tait.djk@gmail.com>
2018-04-22 13:31:09 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pymaniprob',
+      version='0.1',
+      author='Daniel Tait',
+      license='MIT',
+      packages=['pymaniprob'],
+      zip_safe=False)
