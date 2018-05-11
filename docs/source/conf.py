5bf8903c534fac8e12fbfb8d6da9a9e0a18cebb6
danieljtait <tait.djk@gmail.com>
2018-05-12 00:36:29 +0100

    sphinxext for mpl

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,6 +38,8 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+sys.path.append(os.path.abspath('sphinxext'))
+
 extensions = [
     'matplotlib.sphinxext.mathmpl',
     'matplotlib.sphinxext.only_directives',
