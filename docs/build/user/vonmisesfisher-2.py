0c47fb15d06d99786fca421522c7343d15f9c5e7
danieljtait <tait.djk@gmail.com>
2018-05-12 11:56:39 +0100

    rtfd-requ

--- docs/build/user/vonmisesfisher-2.py
+++ docs/build/user/vonmisesfisher-2.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-#from pymaniprob.sphere import VonMisesFisher
-u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
-x = np.cos(u)*np.sin(v)
-y = np.sin(u)*np.sin(v)
-z = np.cos(v)
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.set_aspect("equal")
-ax.plot_wireframe(x, y, z, color="k", alpha=0.2)
+# Build a vector of 10000 normal deviates with variance 0.5^2 and mean 2
+mu, sigma = 2, 0.5
+v = np.random.normal(mu,sigma,10000)
+# Plot a normalized histogram with 50 bins
+plt.hist(v, bins=50, density=1)       # matplotlib version (plot)
+plt.show()
+# Compute the histogram with numpy and then plot it
+(n, bins) = np.histogram(v, bins=50, density=True)  # NumPy version (no plot)
+plt.plot(.5*(bins[1:]+bins[:-1]), n)
 plt.show()
