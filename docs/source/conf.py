84ac09726a683da59c5c1c13cce30217b6e0c222
danieljtait <tait.djk@gmail.com>
2018-05-12 01:10:28 +0100

    added matplotlib sphinxext

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,7 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-# sys.path.append(os.path.abspath('sphinxext'))
+sys.path.append(os.path.abspath('sphinxext'))
 
 
 #extensions = [
@@ -52,13 +52,11 @@ release = '0.0.1'
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['matplotlib.sphinxext.only_directives',
               'matplotlib.sphinxext.plot_directive',
-              'IPython.sphinxext.ipython_directive',
-              'IPython.sphinxext.ipython_console_highlighting',
               'sphinx.ext.mathjax',
               'sphinx.ext.autodoc',
               'sphinx.ext.doctest',
-              'sphinx.ext.inheritance_diagram',
-              'numpydoc']
+              'sphinx.ext.inheritance_diagram']
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
