7ee2c62ea7573c4e74a85de77298be541cf2b747
danieljtait <tait.djk@gmail.com>
2018-05-12 02:17:41 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='pymaniprob',
-      version='0.1dev',
+      version='0.1.1dev',
       author='Daniel Tait',
       email='tait.djk@gmail.com',
       url='http://github.com/danieljtait/pymaniprob',
       license='MIT',
       packages=['pymaniprob'],
-      install_requires=['numpy', 'matplotlib'],
+      install_requires=['numpydoc', 'numpy', 'matplotlib'],
       zip_safe=False)
